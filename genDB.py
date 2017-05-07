09aa49d26a173ecb3d763f4399c1d36062a1f2de
NxCOS <steve.m@chooseopensource.com>
2017-05-07 17:49:01 -0600

    (sp) correction

--- genDB.py
+++ genDB.py
@@ -2,8 +2,7 @@
 This program creates the database that will be used for the main Program.
 It will prompt for username, password and database location/parms so it
 is easy to access FOR TESTING PURPOSES ONLY!! 
-
-Version: 0.0.0.2
+Version: 0.0.0.3
 """
 
 import pymysql
@@ -30,7 +29,12 @@ def createDatabase(hostAddr, dbName, dbUser, dbPass):
     if (checkDatabase(dbCursor, dbName)) == False:
       createUserDefined(dbCursor, dbName, 'no') # Passing 'no' to NOT drop table
     else: # If Database does exits ask user if they wish to rebuild
-      print("Database NOT Dropped, Checking tables!...")
+      userInput = input("Would you like to re-create the database (y/n)?: ").lower()
+      if userInput in ('y', 'yes'):
+        print("OK!...")
+        createUserDefined(dbCursor, dbName, 'DROP')
+      else:
+        print("Database NOT Dropped, Checking tables!...")
     dbLink.close()
 
   # Reopen Database connection and connect to newly created Database
@@ -42,10 +46,10 @@ def createDatabase(hostAddr, dbName, dbUser, dbPass):
     if (checkTable(dbCursor, "gametitle") == False):
     # Create the table and its parms
       dbSQL = """
-                CREATE TABLE IF NOT EXISTS `gametitel` (
+                CREATE TABLE IF NOT EXISTS `gametitle` (
                     `gameName` varchar(45) NOT NULL PRIMARY KEY COMMENT 'GameTitle',  
-                    `portTCPNumbers` int(5) NOT NULL COMMENT 'Server TCP listen ports',  
-                    `portUDPNumbers` int(5) NOT NULL COMMENT 'Server UDP listen ports')"""
+                    `portTCPNumbers` varchar(28) NOT NULL COMMENT 'Server TCP listen ports',  
+                    `portUDPNumbers` varchar(28) NOT NULL COMMENT 'Server UDP listen ports')"""
       dbCursor.execute(dbSQL)
       print("Table gametitle created!...")
 
@@ -75,10 +79,10 @@ def createUserDefined(cursor, databaseName, input):
   # If the user wants rebuild, drop first
   if (input == 'DROP'):
     print("Dropping Database!")
-    dbSQL = "DROP DATABASE '%s'"%(databaseName)
-    Cursor.execute(dbSQL)
+    dbSQL = "DROP DATABASE `{0}`".format(databaseName)
+    cursor.execute(dbSQL)
   # Create the Database
-  dbSQL = "CREATE DATABASE IF NOT EXISTS '{0}'".format(databaseName)
+  dbSQL = "CREATE DATABASE IF NOT EXISTS `{0}`".format(databaseName)
   cursor.execute(dbSQL)
   print("Database Created!...")
 

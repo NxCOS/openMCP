55b62dafb600495c18951635f89d69e9bdade111
NxCOS <steve.m@chooseopensource.com>
2017-05-07 21:10:46 -0600

    update Main

--- Main.py
+++ Main.py
@@ -1,4 +1,23 @@
 """
-Run me!
+Primary exec
 """
 
+
+#Configure Database config file
+def saveDBcfg():
+  import saveDBcfg
+  saveDBcfg.main()
+
+#Check if all is working and cofig save was successful
+def checkDB():
+  import sqlAccess
+  sqlAccess.main()
+
+
+def main():
+  saveDBcfg()
+  checkDB()
+  print("EOL")
+
+if __name__ == "__main__":
+  main()
